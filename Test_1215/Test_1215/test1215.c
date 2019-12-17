60fbeebd4d73347d8be983005b043706f5eabca0
jiangjunguo-web <378287140@qq.com>
2019-12-17 14:53:09 +0800

    1

---
+++ Test_1215/Test_1215/test1215.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void reverse_word(char * start, char * end)
+{
+	int len = end - start;
+	int i, j;
+
+	char tmp;
+	for (i = 0, j = len - 1; i < j; i++, j--)
+	{
+		tmp = start[i];
+		start[i] = start[j];
+		start[j] = tmp;
+	}
+}
+
+void reverse(char * src)
+{
+	int i;
+	char * start = src;
+	char * end;
+
+	for (i = 0; src[i]; i++)
+	{
+		if (src[i] == ' ')
+		{
+			end = src + i;
+			reverse_word(start, end);
+			start = end + 1;
+		}
+	}
+	reverse_word(start, src + i);
+	reverse_word(src, src + i);
+}
+
+int main()
+{
+	char test[50] = "I can sing jump rap basketball.";
+	reverse(test);
+	puts(test);
+	system("pause");
+	return 0;
+}
