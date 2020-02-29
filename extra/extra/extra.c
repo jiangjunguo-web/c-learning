242ff1fc176081301c02338243f0111909a23acf
jiangjunguo-web <378287140@qq.com>
2020-02-29 13:57:31 +0800

    1

---
+++ extra/extra/extra.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//1、贪心过河
+//2、背包问题
+//3、直接插入排序
+//4、希尔排序
+//5、搬石子
+//6、地图柱子
+
+
+//插入排序
+void InsertSort(int * arr[],int n) {
+	for (int i = 1; i < n; i++) {
+	
+	
+	}
+}
+int main() {
+	int m, n;
+	scanf("%d %d",&m,&n);
+	int arr[20];
+	for (int i = 0; i < n - 1; i++) {
+		scanf("%d",&arr[i]);
+	}
+	InsertSort();
+
+}
