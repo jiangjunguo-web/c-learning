70d56b8c1b34bfc0052b6ede366ff477144bc9a7
jiangjunguo-web <378287140@qq.com>
2020-03-07 14:46:37 +0800

    实现字符串函数： 拼接 复制 求长度

---
+++ Test0307/Test0307/Test0307.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//自创建strcat函数
+char* Strcat(char* destination, char* str) {
+	if (destination == NULL || str == NULL) {
+		return NULL;
+	}
+	else {
+		int count = 0;
+		while (destination[count] != 0) {
+			count++;
+		}
+		int i = 0;
+		while (str[i] != 0) {
+			destination[count + i] = str[i];
+			i++;
+		}
+		destination[i+count] = 0;
+		return destination;
+	}
+}
+
+//自创建函数Strcpy
+char* Strcpy(char* str, char* arr) {
+	if (str == NULL || arr == NULL) {
+		return NULL;
+	}
+	else {
+		char* begin = str;
+		while (*arr != 0) {
+			*str = *arr;
+			str++;
+			arr++;
+		}
+		*str = 0;
+		return begin;
+	}
+}
+
+//自创建Strlen函数
+int Strlen(char* str) {
+	if (str == NULL) {
+		return 0;
+	}
+	else {
+		int count = 0;
+		while (*str != 0) {
+			str++;
+			count++;
+		}
+		return count;
+	}
+}
+int main() { 
+	char str1[200] = "haha";
+	char str2[200] = "heheheuiheii";
+	printf("%d\n", Strlen(str1));
+	printf("%d\n", Strlen(str2));
+	system("pause");
+	return 0;
+}
